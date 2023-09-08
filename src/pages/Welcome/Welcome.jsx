913e0c0a41c3bc621477a64a945f427c5bf5051f
antidoid <singhpiyush998@gmail.com>
2023-09-08 21:16:38 +0530

    Fix: Link to authselection

--- src/pages/Welcome/Welcome.jsx
+++ src/pages/Welcome/Welcome.jsx
@@ -1,4 +1,5 @@
 import getImgUrl from "../../utils/imageSrc";
+import { Link } from "react-router-dom";
 import "./Welcome.css";
 
 export default function Welcome() {
@@ -14,9 +15,11 @@ export default function Welcome() {
       <div className="text-7xl text-white italic font-bold m-8">
         NOTE <span className="font-normal">TAKE</span> PLUS
       </div>
-      <button className="self-end bg-white mb-50 mr-10 rounded-full font-bold italic h-16 w-60 hover:bg-slate-300">
-        LETS GET STARTED
-      </button>
+      <Link to={"auth"} className="self-end">
+        <button className="bg-white mb-50 mr-10 rounded-full font-extrabold text-lg italic h-16 w-60 hover:bg-slate-300">
+          LETS GET STARTED
+        </button>
+      </Link>
     </div>
   );
 }
